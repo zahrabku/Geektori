05810188297addea7b46872a1e8f9eb6c97b2bfd
zahrabku <zahrabku>
2021-08-10 15:16:55 +0430

    add container add colors

--- geektori/src/App.js
+++ geektori/src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
+import Container from './components/container.js'
+import './styles/sass/container__body.scss'
 
 function App() {
+  return(
+      <Container className={'container__body'}>
+        
+      </Container>
 
+      
+    
+  );
 }
 
 export default App;
---
+++ geektori/src/components/container.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/sass/container__body.scss'
+
+
+
+function Container(props){
+    return (
+        <div className={props.className} >
+            {props.children}
+        </div>
+    );
+}
+
+export default Container;
