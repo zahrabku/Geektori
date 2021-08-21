1963283633f61d1ef2953c1a1c996356652848d4
Negar <negar.r.78.ioi@gmail.com>
2021-08-21 15:41:14 +0430

    separate data

---
+++ geektori/src/components/DataDump.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { MenuItemIcon } from "./Menu";
+
+import {
+	FaAngleDown,
+	FaShoppingBasket,
+	FaHeart,
+	FaSearch,
+} from "react-icons/fa";
+
+import Vanilla from "../images/vanilla.png";
+import Lama from "../images/lama.png";
+import Octopus from "../images/octopus.jpg";
+import Penguin from "../images/penguin.png";
+
+interface MenuCategories {
+	name: string;
+	icon?: any;
+	classNameStyle?: string;
+}
+
+interface CardItems {
+	image: string;
+	imageAlt?: string;
+	title: string;
+	existance: boolean;
+	price: number;
+}
+
+const rightMenuCategories: MenuCategories[] = [
+	{
+		name: "استیکر",
+		icon: <MenuItemIcon iconName={FaAngleDown} />,
+	},
+	{
+		name: "استیکرهای فانتزی",
+		icon: <MenuItemIcon iconName={FaAngleDown} />,
+	},
+	{
+		name: "طراح‌هامون",
+		icon: <MenuItemIcon iconName={FaHeart} />,
+	},
+	{
+		name: " طرح دلخواه",
+	},
+	{
+		name: "پینتوری",
+	},
+];
+
+const leftMenuCategories: Partial<MenuCategories>[] = [
+	{
+		icon: <MenuItemIcon iconName={FaSearch} className='search-icon' />,
+	},
+	{
+		name: "سبد خرید",
+		icon: (
+			<MenuItemIcon
+				iconName={FaShoppingBasket}
+				className='shopping-basket-icon'
+			/>
+		),
+		classNameStyle: "shopping-basket",
+	},
+	{
+		name: "عضویت",
+	},
+	{
+		name: "وارد شوید",
+	},
+];
+
+const items: CardItems[] = [
+	{
+		image: Penguin,
+		imageAlt: "penguin",
+		title: "Penguin استیکر",
+		existance: false,
+		price: 2000,
+	},
+	{
+		image: Vanilla,
+		imageAlt: "penguin",
+		title: "Penguin استیکر",
+		existance: false,
+		price: 2000,
+	},
+	{
+		image: Penguin,
+		imageAlt: "penguin",
+		title: "Penguin استیکر",
+		existance: false,
+		price: 2000,
+	},
+	{
+		image: Octopus,
+		imageAlt: "penguin",
+		title: "Penguin استیکر",
+		existance: false,
+		price: 2000,
+	},
+	{
+		image: Lama,
+		imageAlt: "penguin",
+		title: "Penguin استیکر",
+		existance: false,
+		price: 2000,
+	},
+];
+
+export { rightMenuCategories, leftMenuCategories, items };
