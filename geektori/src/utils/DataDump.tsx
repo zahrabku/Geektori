cf2a1537a5cb5337248cc599bbb51432b42da67d
Negar <negar.r.78.ioi@gmail.com>
2021-08-29 17:55:01 +0430

    make cards's detail more clear

--- geektori/src/utils/DataDump.tsx
+++ geektori/src/utils/DataDump.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { MenuItemIcon } from "../components/Menu";
 import {
-  FaAngleDown,
-  FaShoppingBasket,
-  FaHeart,
-  FaSearch,
+	FaAngleDown,
+	FaShoppingBasket,
+	FaHeart,
+	FaSearch,
 } from "react-icons/fa";
 import Vanilla from "../images/vanilla.png";
 import Lama from "../images/lama.png";
@@ -12,112 +12,103 @@ import Octopus from "../images/octopus.jpg";
 import Penguin from "../images/penguin.png";
 
 interface MenuCategories {
-  name: string;
-  icon?: any;
-  classNameStyle?: string;
-  click?: () => void;
+	name: string;
+	icon?: any;
+	classNameStyle?: string;
+	click?: () => void;
 }
 
 export interface CardItems {
-  id: number;
-  image: string;
-  imageAlt?: string;
-  title: string;
-  existance: boolean;
-  price: number;
-  amount: number;
+	id: number;
+	image: string;
+	imageAlt?: string;
+	title: string;
+	existance: boolean;
+	price: number;
+	amount: number;
 }
 
 const rightMenuCategories: MenuCategories[] = [
-  {
-    name: "استیکر",
-    icon: <MenuItemIcon iconName={FaAngleDown} />,
-  },
-  {
-    name: "استیکرهای فانتزی",
-    icon: <MenuItemIcon iconName={FaAngleDown} />,
-  },
-  {
-    name: "طراح‌هامون",
-    icon: <MenuItemIcon iconName={FaHeart} />,
-  },
-  {
-    name: " طرح دلخواه",
-  },
-  {
-    name: "پینتوری",
-  },
+	{
+		name: "استیکر",
+		icon: <MenuItemIcon iconName={FaAngleDown} />,
+	},
+	{
+		name: "استیکرهای فانتزی",
+		icon: <MenuItemIcon iconName={FaAngleDown} />,
+	},
+	{
+		name: "طراح‌هامون",
+		icon: <MenuItemIcon iconName={FaHeart} />,
+	},
+	{
+		name: " طرح دلخواه",
+	},
+	{
+		name: "پینتوری",
+	},
 ];
 
 const leftMenuCategories: Partial<MenuCategories>[] = [
-  {
-    icon: <MenuItemIcon iconName={FaSearch} className="search-icon" />,
-  },
-  {
-    name: "سبد خرید",
-    icon: (
-      <MenuItemIcon
-        iconName={FaShoppingBasket}
-        className="shopping-basket-icon"
-      />
-    ),
-    classNameStyle: "shopping-basket",
-    // click:useData()!.addShoppingCartModalIsOpen
-  },
-  {
-    name: "عضویت",
-  },
-  {
-    name: "وارد شوید",
-  },
+	{
+		icon: <MenuItemIcon iconName={FaSearch} className='search-icon' />,
+	},
+	{
+		name: "سبد خرید",
+		icon: (
+			<MenuItemIcon
+				iconName={FaShoppingBasket}
+				className='shopping-basket-icon'
+			/>
+		),
+		classNameStyle: "shopping-basket",
+		// click:useData()!.addShoppingCartModalIsOpen
+	},
+	{
+		name: "عضویت",
+	},
+	{
+		name: "وارد شوید",
+	},
 ];
 
 const items: CardItems[] = [
-  {
-    id: 1,
-    image: Penguin,
-    imageAlt: "penguin",
-    title: "Penguin استیکر",
-    existance: false,
-    price: 1000,
-    amount: 0,
-  },
-  {
-    id: 2,
-    image: Vanilla,
-    imageAlt: "penguin",
-    title: "Penguin استیکر",
-    existance: true,
-    price: 2000,
-    amount: 0,
-  },
-  {
-    id: 3,
-    image: Penguin,
-    imageAlt: "penguin",
-    title: "Penguin استیکر",
-    existance: true,
-    price: 2200,
-    amount: 0,
-  },
-  {
-    id: 4,
-    image: Octopus,
-    imageAlt: "penguin",
-    title: "Penguin استیکر",
-    existance: false,
-    price: 3000,
-    amount: 0,
-  },
-  {
-    id: 5,
-    image: Lama,
-    imageAlt: "penguin",
-    title: "Penguin استیکر",
-    existance: true,
-    price: 2500,
-    amount: 0,
-  },
+	{
+		id: 1,
+		image: Penguin,
+		imageAlt: "penguin",
+		title: "استیکر Penguin",
+		existance: false,
+		price: 1000,
+		amount: 0,
+	},
+	{
+		id: 2,
+		image: Vanilla,
+		imageAlt: "vanilla",
+		title: "استیکر Vanilla",
+		existance: true,
+		price: 2000,
+		amount: 0,
+	},
+	{
+		id: 3,
+		image: Octopus,
+		imageAlt: "octopus",
+		title: "استیکر Octopus",
+		existance: false,
+		price: 3000,
+		amount: 0,
+	},
+	{
+		id: 4,
+		image: Lama,
+		imageAlt: "lama",
+		title: "استیکر Lama",
+		existance: true,
+		price: 2500,
+		amount: 0,
+	},
 ];
 
 export { rightMenuCategories, leftMenuCategories, items };
